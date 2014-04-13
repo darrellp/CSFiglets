a7f1ef67d89a405c8554abef3916f3b3f1b27c81
darrellp <darrellp@b8047da4-bbf6-4fb8-b13b-4a98fbbd6f4a>
2014-04-13 16:48:51 +0000

    Started implementation of wrapping

--- CSFiglet/Arranger.cs
+++ CSFiglet/Arranger.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CSFiglet
@@ -6,37 +8,42 @@ namespace CSFiglet
 	public class Arranger
 	{
 		#region Private variables
-		private readonly List<StringBuilder> _contents;
+		private List<string> _contents;
+		private readonly List<StringBuilder> _stagingArea; 
 		private readonly FigletFont _font;
+		private string _text;
 		#endregion
 
 		#region Properties
-		public CharacterSpacing CharacterSpacing { get; set; }
-		public HSmushRule SmushRule { get; set; }
-
-		public bool Smush
+		public bool DoSmush
 		{
 			get
 			{
 				return SmushRule != HSmushRule.NoSmush;
 			}
 		}
+
 		public string StringContents
 		{
 			get
 			{
 				var sb = new StringBuilder();
-				for (var iRow = 0; iRow < Rows; iRow++)
+				for (var iRow = 0; iRow < _contents.Count; iRow++)
 				{
-					sb.Append(_contents[iRow] + (iRow == Rows - 1 ? "" : "\n"));
+					sb.Append(_contents[iRow] + (iRow == _contents.Count - 1 ? "" : "\n"));
 				}
 				return sb.ToString().Replace(_font.Header.HardBlank, ' ');
 			}
 		}
-		private int Rows { get; set; }
-		private int Columns { get; set; }
 
-		private string _text;
+		public string[] Contents
+		{
+			get
+			{
+				return _contents.Select(l => l.Length < MaxWidth ? l : l.Substring(0, MaxWidth)).ToArray();
+			}
+		}
+
 		public string Text
 		{
 			get
@@ -50,18 +57,34 @@ namespace CSFiglet
 			}
 		}
 
+		public CharacterSpacing CharacterSpacing { get; set; }
+		public HSmushRule SmushRule { get; set; }
+
+		private int MaxWidth { get; set; }
+		#endregion
+
+		#region Constructor
+		public Arranger(FigletFont font, int maxWidth = int.MaxValue)
+		{
+			_stagingArea = new List<StringBuilder>();
+			_font = font;
+			MaxWidth = maxWidth;
+			SetCharSpacing();
+			for (var iRow = 0; iRow < _font.Header.Height; iRow++)
+			{
+				_stagingArea.Add(new StringBuilder());
+			}
+		}
+		#endregion
+
+		#region Arrangement
 		private void Arrange()
 		{
-			Rows = _font.Header.Height;
-	
-			for (var iRow = 0; iRow < Rows; iRow++)
-			{
-				_contents.Add(new StringBuilder());
-			}
-
+			_contents = new List<string>();
 			var lShiftCur = 0;							// Amount to "shift" right char into the left one
 			var chPrev = (char)0;
 			var rightBorder = 0;
+			var stagedCharCount = 0;
 			foreach(var ch in _text)
 			{
 				var curChar = _font.Chars[ch];
@@ -75,26 +98,45 @@ namespace CSFiglet
 				else if (chPrev != 0)
 				{
 					lShiftCur = _font.Chars[chPrev].KerningOffset(curChar, SmushRule, out smushable);
-					if (smushable && Smush)
+					if (smushable && DoSmush)
 					{
 						lShiftCur++;
 					}
 				}
-				SetText(0, rightBorder - lShiftCur, rightBorder, curChar, Smush && smushable);
+
+				// Have we extended beyond the right border with more than one character?
+				if (rightBorder + curChar.Width - lShiftCur > MaxWidth && stagedCharCount != 0)
+				{
+					// Yes, move the staging text to contents
+					TransferStagingToContents(0);
+					// ...and clear the staging area
+					foreach (var stagingRow in _stagingArea)
+					{
+						stagingRow.Clear();
+					}
+					stagedCharCount = 0;
+					lShiftCur = 0;
+					chPrev = (char) 0;
+					rightBorder = 0;
+				}
+				stagedCharCount++;
+				SetText(0, rightBorder - lShiftCur, rightBorder, curChar, DoSmush && smushable);
 				rightBorder += curChar.Width - lShiftCur;
 				chPrev = ch;
 			}
+			TransferStagingToContents(0);
 		}
-		#endregion
 
-		#region Constructor
-		public Arranger(FigletFont font, int rows = -1, int cols = -1)
+		private void TransferStagingToContents(int padding)
 		{
-			Rows = rows;
-			Columns = cols;
-			_contents = new List<StringBuilder>();
-			_font = font;
-			SetCharSpacing();
+			var paddingString = padding > 0 ? new string(' ', padding) : string.Empty;
+
+			foreach (var row in _stagingArea)
+			{
+				var justifiedLine = paddingString + (padding > 0 ? row.ToString() : row.ToString().Substring(-padding));
+				var clippedLine = justifiedLine.Length <= MaxWidth ? justifiedLine : justifiedLine.Substring(0, MaxWidth);
+				_contents.Add(clippedLine);
+			}
 		}
 
 		public void SetCharSpacing()
@@ -117,9 +159,7 @@ namespace CSFiglet
 					_font.Header.OldLayout;
 			}
 		}
-		#endregion
 
-		#region Block Get/Set
 		internal void SetText(int row, int column, int curWidth, CharInfo charInfo, bool doSmush)
 		{
 			var rowCount = charInfo.SubChars.Count;
@@ -132,7 +172,7 @@ namespace CSFiglet
 				{
 					// lPadding == -1 conventionally means no subchars in this row so we just
 					// append on the proper amount of spacing and continue on
-					_contents[iRow].Append(new string(' ', charInfo.Width - curWidth + column));
+					_stagingArea[iRow].Append(new string(' ', charInfo.Width - curWidth + column));
 					continue;
 				}
 
@@ -143,25 +183,26 @@ namespace CSFiglet
 
 				if (curWidth > colStart)
 				{
-					// We have to actually replace part of the last character we've already placed
+					// We have to actually replace part of the last character we've already placed - i.e., we're
+					// kerning into the body of the previous character.
 					if (doSmush)
 					{
-						// Left char to smush comes from the contents
-						var lSmushChar = _contents[iRow][colStart];
-						// Right char to smuch comes from replacement text
+						// Left char to smush comes from the stagingArea
+						var lSmushChar = _stagingArea[iRow][colStart];
+						// Right char to smush comes from replacement text
 						var rSmushChar = replacementText[lPadding];
 
-						// Replace _contents char with the smushability char
-						_contents[iRow][colStart] = 
+						// Replace _stagingArea char with the smushability char
+						_stagingArea[iRow][colStart] = 
 							CharInfo.CheckSmushability(SmushRule, lSmushChar, rSmushChar, _font.Header.HardBlank);
 
 						// This will cause the smushed char to be skipped in replacement text
 						lPadding++;
-						// This will keep us from deleting the newly placed smush char from _contents
+						// This will keep us from deleting the newly placed smush char from _stagingArea
 						colStart++;
 					}
 					// Eliminate blanks that need replacing
-					_contents[iRow].Remove(colStart, curWidth - colStart);
+					_stagingArea[iRow].Remove(colStart, curWidth - colStart);
 				}
 				else
 				{
@@ -171,7 +212,7 @@ namespace CSFiglet
 				}
 				
 				// Put the replacement text in properly
-				_contents[iRow].Append(replacementText.Substring(lPadding));
+				_stagingArea[iRow].Append(replacementText.Substring(lPadding));
 			}
 		}
 		#endregion
